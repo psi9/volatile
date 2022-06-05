784d185d9d8b7b07c73e9f51310d7d8470471b24
psi9 <semyon.pavlenko@gmail.com>
2022-06-05 19:28:36 +1000

    Add project files.

---
+++ volatile2/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Threading;
+
+namespace volatile2
+{
+    class Program
+    {
+
+
+        public class Example1
+        {
+            private int flag = 0;
+            private int value = 0;
+            public void Thread1()
+            {
+                flag = 1;
+                value = 5;
+            }
+
+            public void Thread2()
+            {
+                if (flag == 1)
+                    Console.WriteLine("e1 " + value);
+                else
+                    Console.WriteLine("e1 fail");
+            }
+        }
+
+
+        public class Example2
+        {
+            private int flag = 1;
+            private int value = 0;
+            public void Thread1()
+            {
+                value = 5;
+                Volatile.Write(ref flag, 1);
+            }
+
+            public void Thread2()
+            {
+                if (Volatile.Read(ref flag) == 1) 
+                    Console.WriteLine("e2 " + value);
+                else
+                    Console.WriteLine("e2 fail");
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var e1 = new Example1();
+            Thread thread11 = new Thread(new ThreadStart(e1.Thread1));
+            Thread thread12 = new Thread(new ThreadStart(e1.Thread2));
+
+            var e2 = new Example2();
+            Thread thread21 = new Thread(new ThreadStart(e2.Thread1));
+            Thread thread22 = new Thread(new ThreadStart(e2.Thread2));
+
+            thread11.Start();
+            thread12.Start();
+
+            thread21.Start();
+            thread22.Start();
+        }
+    }
+}
